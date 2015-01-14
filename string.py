df73bc1ee3bfc255978c6287bf7e287bca0c02ab
Yoichi Nakayama <yoichi.nakayama@gmail.com>
2015-01-14 13:11:18 +0900

    ignore whitespaces

--- string.py
+++ string.py
@@ -1,13 +1,14 @@
 class String:
     def __init__(self, data):
-        self.__data = data
+        self.__data = data.replace(" ", "")
     def get(self):
         return self.__data
     def equals(self, other):
-        return self.__data == other
+        return self.__data == other.replace(" ", "")
 
 if __name__ == '__main__':
     a = String("Example String")
-    assert a.get() == "Example String"
+    assert a.get() == "ExampleString"
     assert a.equals("Example String") == True
+    assert a.equals("ExampleString") == True
     assert a.equals("Different String") == False
