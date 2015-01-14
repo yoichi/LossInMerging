7e0bfe1f4688847d75638bb819793dc5790b9675
Yoichi Nakayama <yoichi.nakayama@gmail.com>
2015-01-14 13:11:18 +0900

    ignore whitespaces
    Conflicts:
    	string.py

--- string.py
+++ string.py
@@ -1,9 +1,9 @@
 class String:
     def __init__(self, data):
-        self.__data = data
+        self.__data = data.replace(" ", "")
     def get(self):
         return self.__data
 
 if __name__ == '__main__':
     a = String("Example String")
-    assert a.get() == "Example String"
+    assert a.get() == "ExampleString"
