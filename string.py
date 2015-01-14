f53bed867a38722a6b9af86ed474648da0215aed
Yoichi Nakayama <yoichi.nakayama@gmail.com>
2015-01-14 13:05:58 +0900

    define equals method

--- string.py
+++ string.py
@@ -3,7 +3,11 @@ class String:
         self.__data = data
     def get(self):
         return self.__data
+    def equals(self, other):
+        return self.__data == other
 
 if __name__ == '__main__':
     a = String("Example String")
     assert a.get() == "Example String"
+    assert a.equals("Example String") == True
+    assert a.equals("Different String") == False
