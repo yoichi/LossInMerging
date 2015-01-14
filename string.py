de139f37a76694f2829e1e87fd1a67b7f77d8e29
Yoichi Nakayama <yoichi.nakayama@gmail.com>
2015-01-14 13:11:18 +0900

    ignore whitespaces

--- string.py
+++ string.py
@@ -1,7 +1,7 @@
 class String:
     def __init__(self, data):
-        self.__data = data
+        self.__data = data.replace(" ", "")
     def get(self):
         return self.__data
     def equals(self, other):
-        return self.__data == other
+        return self.__data == other.replace(" ", "")
